69ae6ec923645c520baa823317eeef894e85b28e
Wenhao Zhao <zhaowh1987@gmail.com>
2020-04-29 20:36:04 +0800

    add call spdier interval

--- flask_app/manage.py
+++ flask_app/manage.py
@@ -15,8 +15,36 @@
 from app import app
 from flask import Flask
 from flask_script import Manager
+from flask_apscheduler import APScheduler
+
+import time
+import sys
+import os 
+import path
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../news_comment_spider')))
+#from runspider import RunSpider
+from scrapy import cmdline
 
 manager = Manager(app)
+scheduler = APScheduler(app=app)
+
+@app.before_first_request
+@scheduler.task(trigger='interval', id='run_spider', seconds=100)
+def run_spider():
+    cwd = os.getcwd()
+    print(f"current working dir {cwd}")
+    print(f'running spider @ {time.asctime(time.localtime(time.time()))}')
+    spider_workding_dir = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '../news_comment_spider')))
+    print(f"change dir {spider_workding_dir}")
+    os.chdir(spider_workding_dir)
+    os.system("scrapy crawl sina_news_comment")
+    os.chdir(cwd)
+    print(f"reset dir: {os.getcwd()}")
+
+
+    
+    
 
 @manager.command
 def dev():
@@ -26,4 +54,5 @@ def dev():
     live_server.serve(open_url=True)
 
 if __name__ == "__main__":
+    scheduler.start()
     manager.run()
