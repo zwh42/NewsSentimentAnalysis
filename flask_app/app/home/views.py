a05a296f13cfdf42bd5eadb6aaa7fd0c68450621
Wenhao Zhao <zhaowh1987@gmail.com>
2020-04-29 17:23:42 +0800

    add simple flask demo page

---
+++ flask_app/app/home/views.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from . import home
+from app.models import *
+from app import db
+
+
+@home.route('/')
+@home.route('/index')
+def result():
+        #shorts = T1.query.all()[0:10]
+        #shorts = T1.query.order_by(T1.sentiment.desc())[:10]
+        comments_data = CommmetsData.query.all()[:10]
+        return render_template('/home/result.html', comments_data=comments_data)
+
+
+
+
+
+
