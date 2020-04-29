4cb99967a0aa2e96bcb8c0f3c8ca03e29fb83949
Wenhao Zhao <zhaowh1987@gmail.com>
2020-04-29 17:40:39 +0800

    minor modification

--- news_comment_spider/news_comment_spider/settings.py
+++ news_comment_spider/news_comment_spider/settings.py
@@ -80,6 +80,7 @@ ROBOTSTXT_OBEY = True
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'news_comment_spider.pipelines.NewsCommentSpiderPipeline': 300,
+    'news_comment_spider.pipelines.SentimentCalculationPipeline': 400,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
